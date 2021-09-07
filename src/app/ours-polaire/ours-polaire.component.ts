d4a43dbce73f45139bbe43d7f18cb7f68edc1729
Lagniaux Jean <34351725+jeanlagniaux@users.noreply.github.com>
2021-09-07 22:20:43 +0200

    add video and image to article component

--- src/app/ours-polaire/ours-polaire.component.ts
+++ src/app/ours-polaire/ours-polaire.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ours-polaire',
   templateUrl: './ours-polaire.component.html',
   styleUrls: ['./ours-polaire.component.scss']
 })
+
 export class OursPolaireComponent implements OnInit {
 
-  constructor() { }
+  public href: string = "";
+  public src: string = 'assets/\img/\CONTOURPHOTOalphav.png';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.href = this.router.url;
+    console.log(this.router.url);
   }
+
 
 }
